e8b4c6c158e84d968f4eae755099411b40a820ca
mirea70 <deae670@daum.net>
2025-12-23 21:38:33 +0900

    Initial Commit

---
+++ modules/adapter/src/main/java/com/wirebarly/WirebarlyApp.java
@@ -0,0 +1,11 @@
+package com.wirebarly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WirebarlyApp {
+    public static void main(String[] args) {
+        SpringApplication.run(WirebarlyApp.class, args);
+    }
+}
